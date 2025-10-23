aeea99632f516c9cc61ab4ae8e1f310292003ecb
cadst <tyu1234567@naver.com>
2025-10-23 15:36:12 +0900

    [fix] About 페이지에서 다른 페이지로 넘어가지 않는 오류 수정

--- src/pages/dev/DevCenter.jsx
+++ src/pages/dev/DevCenter.jsx
@@ -7,10 +7,14 @@ import DevCenterCta from '@/components/devcenter/DevCenterCta';
 
 import { useSelector } from 'react-redux';
 import { TContext } from '@/contexts/TContext';
+import useScrollToTop from '@/hooks/useScrollToTop';
 
 export default function DevCenter() {
   const lan = useSelector((state) => state.lan.ver);
   const pageKey = 'devcenter';
+
+  // 페이지 이동 시 스크롤을 맨 위로 이동
+  useScrollToTop();
 
   return (
     <TContext.Provider value={{ pageKey }}>
